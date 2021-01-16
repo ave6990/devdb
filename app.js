e92821d7a37d4bab67dccae9d09fb1cbf888243e
ave-student <free-is@ya.ru>
2021-01-16 12:04:38 +0500

    date

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const port = 3000
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-	res.render('index', {title: 'Hey', message: 'My test app!!! (ave6990)'})
+	res.render('index', {title: 'Metrolog', 
+		header: 'My test app!!!',
+		content: 'Here must be content...',
+		copyright: 'ave6990@ya.ru 2021'})
 })
 
 app.get('/ggs', (req, res) => {
