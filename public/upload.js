e64d1103093b4cd9d73bf6b3cdd00f0377ae1568
ave <ave6990@ya.ru>
2021-02-02 04:51:13 +0000

    mongodb connection

--- public/upload.js
+++ public/upload.js
@@ -4,14 +4,14 @@ const button = form.elements.btn_upload
 button.onclick = () => {
 	let file = document.getElementById('file').files[0]
 	file.text().then((res, err) => {
-		console.log(res)
 		$.ajax({
 			url: 'upload',
 			type: 'POST',
 			cache: false,
 			dataType: 'json',
 			contentType: 'application/json',
-			data: JSON.stringify({text: res}),
+			encoding: 'utf-8',
+			data: JSON.stringify({csv: res}),
 			success: (data) => {
 				console.log('Successfull upload.')
 			},
---
+++ routes/upload.js
@@ -0,0 +1,3 @@
+const csv = require('csv')
+
+
