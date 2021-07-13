4f4b49a3f1c9677c4650f4b2a4bcc45e93771383
ave6990 <ave6990@ya.ru>
2021-07-13 06:23:49 +0000

    v0.4.3
     On branch dev
     Changes to be committed:
    	modified:   README.md
    	deleted:    public/ui.js

--- public/ui.js
+++
@@ -1,8 +0,0 @@
-// dependece: JQuery
-
-const addInputField = (parrent, name, title='', value='') => {
-	if (title != '') {
-		parrent.append(`<label>${title}<label>`)
-	}
-	parrent.append(`<input type='text' id=${name} value=${value}`)
-}
